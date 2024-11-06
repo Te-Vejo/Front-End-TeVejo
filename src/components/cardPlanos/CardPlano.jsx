0c2a4d0523a9113db94785e3e468daef6c5b58ac
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-11-05 23:47:46 -0300

    home acabada - sem style e sem responsividade

--- src/components/cardPlanos/CardPlano.jsx
+++ src/components/cardPlanos/CardPlano.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import './style.css'
+import { Link } from 'react-router-dom';
 
 const CardPlano = (props) => {
   return (
       <div className="book" style={{backgroundColor:props.CorfundoCartao02}}>
-        <p className='ConteudoPlano'>{props.conteudo}</p>
+        <div className='ConteudoPlano'>
+          <h5 className='tituloCardPlano'>Benefícios</h5>
+          <ul className='ListaCardPlano'>
+            <li>{props.bene01}</li>
+            <li>{props.bene02}</li>
+            <li>{props.bene03}</li>
+            <li>{props.bene04}</li>
+          </ul>
+          
+          {<Link to={props.caminhoBtn} className='LinkCardPlano'>{props.txtBtn}</Link>}
+
+        </div>
         <div className="cover" style={{background:props.CorfundoCartao}}>
          <h3 className='TituloPlano'>{props.ValorPlano} 
 
          <p className='TipoPlano'>{props.TipoPlano}</p>
          </h3>
 
-         <p className='PasseMouse'>Passe o mouse por cima!</p>
+         <p className='PasseMouse'>Passe o mouse ou clique!</p>
          
 
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -65,11 +65,12 @@ function Home(){
                 {<Link className='linkParaOsJogosHome' to={"/jogos"}>PARA CONHECER MAIS JOGOS CLIQUE AQUI!</Link>}
         </section>
 
-        <h2 className="SubtituloParaOsJogos">Olhe e escolha o plano</h2>
+        <h2 className="SubtituloParaOsPlanos">Olhe e escolha o plano</h2>
 
         <section className='QuartaSeccaoHome'> 
-            <CardPlano ValorPlano={"R$ 00,00/Mês"} TipoPlano={"Grátis"}/>
-            <CardPlano/>
+            <CardPlano ValorPlano={"R$ 00,00/Mês"} TipoPlano={"Free"} bene01={"Acesso aos blogs"} bene02 = {"LensKids: Teste grátis"} bene03={"2 jogos grátis"} bene04={"Realidade aumentada"} caminhoBtn={"/cadastro"} txtBtn={"Cadastrar-se"}/>
+
+            <CardPlano ValorPlano={"R$ 29,00/Mês"} TipoPlano={"Plus"} bene01={"Sem anúncios"} bene02 = {"LensKids: Ilimitado"} bene03={"Jogos liberados"} bene04={"Ícones exclusivos"} caminhoBtn={"/"} txtBtn={"Assinar agora"}/>
         </section>
         
         <FooterPadrao/>
